2ce19e6417b242a5157981288600f5fb98198ed0
Ankita Todkar <ankiattodkar1010@gmail.com>
2026-08-22 12:59:32 +0530

    Add user model

---
+++ app/models/__init__.py
@@ -0,0 +1,2 @@
+from app.models.incident import Incident
+from app.models.user import User
---
+++ app/models/user.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+
+from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String, nullable=False)
+
+    email = Column(String, unique=True, nullable=False, index=True)
+
+    password_hash = Column(String, nullable=False)
+
+    role = Column(String, nullable=False, default="USER")
+
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
